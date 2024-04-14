2c21a485e1beb87a5688def9d0b5558adeeccc27
Le Duc Phat <phat.lduc@gmail.com>
2024-04-14 23:38:56 +0700

    update

--- main.py
+++ main.py
@@ -14,331 +14,54 @@ import time
 
 ############################################# FUNCTIONS ################################################
 
-def assure_path_exists(path):
-    dir = os.path.dirname(path)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
-
-##################################################################################
-
-def tick():
-    time_string = time.strftime('%H:%M:%S')
-    clock.config(text=time_string)
-    clock.after(200, tick)
-
-
-###################################################################################
-
-def check_haarcascadefile():
-    exists = os.path.isfile("haarcascade_frontalface_default.xml")
-    if exists:
-        pass
-    else:
-        mess._show(title='Error', message='Thiếu file haarcascade.')
-        window.destroy()
-
-
-###################################################################################
-
-def save_pass():
-    assure_path_exists("TrainingImageLabel/")
-    exists1 = os.path.isfile("TrainingImageLabel/psd.txt")
-    if exists1:
-        tf = open("TrainingImageLabel/psd.txt", "r")
-        key = tf.read()
-    else:
-        master.destroy()
-        new_pas = tsd.askstring('Error', 'Hãy nhập mật khẩu mới!', show='*')
-        if new_pas == None:
-            mess._show(title='Error', message='Mật khẩu không hợp lệ. Xin vui lòng thử lại!')
-        else:
-            tf = open("TrainingImageLabel/psd.txt", "w")
-            tf.write(new_pas)
-            mess._show(title='Success', message='Mật khẩu mới đã được cài đặt thành công!')
-            return
-    op = (old.get())
-    newp = (new.get())
-    nnewp = (nnew.get())
-    if (op == key):
-        if (newp == nnewp):
-            txf = open("TrainingImageLabel/psd.txt", "w")
-            txf.write(newp)
-        else:
-            mess._show(title='Error', message='Hãy xác nhận lại mật khẩu!')
-            return
-    else:
-        mess._show(title='Error', message='Hãy nhập chính xác mật khẩu cũ')
-        return
-    mess._show(title='Success', message='Mật khẩu mới đã được cài đặt thành công!')
-    master.destroy()
-
-
-###################################################################################
-
-def change_pass():
-    global master
-    master = tk.Tk()
-    master.geometry("400x160")
-    master.resizable(False, False)
-    master.title("Change Password")
-    master.configure(background="white")
-    lbl4 = tk.Label(master, text='    Nhập mật khẩu cũ', bg='white', font=('comic', 12, ' bold '))
-    lbl4.place(x=10, y=10)
-    global old
-    old = tk.Entry(master, width=25, fg="black", relief='solid', font=('comic', 12, ' bold '), show='*')
-    old.place(x=180, y=10)
-    lbl5 = tk.Label(master, text='   Nhập mật khẩu mới', bg='white', font=('comic', 12, ' bold '))
-    lbl5.place(x=10, y=45)
-    global new
-    new = tk.Entry(master, width=25, fg="black", relief='solid', font=('comic', 12, ' bold '), show='*')
-    new.place(x=180, y=45)
-    lbl6 = tk.Label(master, text='Xác nhận mật khẩu mới', bg='white', font=('comic', 12, ' bold '))
-    lbl6.place(x=10, y=80)
-    global nnew
-    nnew = tk.Entry(master, width=25, fg="black", relief='solid', font=('comic', 12, ' bold '), show='*')
-    nnew.place(x=180, y=80)
-    cancel = tk.Button(master, text="Hủy", command=master.destroy, fg="black", bg="red", height=1, width=25,
-                       activebackground="white", font=('comic', 10, ' bold '))
-    cancel.place(x=200, y=120)
-    save1 = tk.Button(master, text="Lưu", command=save_pass, fg="black", bg="#00fcca", height=1, width=25,
-                      activebackground="white", font=('comic', 10, ' bold '))
-    save1.place(x=10, y=120)
-    master.mainloop()
-
-
-#####################################################################################
-
-def psw():
-    assure_path_exists("TrainingImageLabel/")
-    exists1 = os.path.isfile("TrainingImageLabel/psd.txt")
-    if exists1:
-        tf = open("TrainingImageLabel/psd.txt", "r")
-        key = tf.read()
-    else:
-        new_pas = tsd.askstring('Error', 'Xin vui lòng nhập mật khẩu mới bên dưới', show='*')
-        if new_pas == None:
-            mess._show(title='Error', message='Mật khẩu không hợp lệ. Xin vui lòng thử lại!')
-        else:
-            tf = open("TrainingImageLabel/psd.txt", "w")
-            tf.write(new_pas)
-            mess._show(title='Success', message='Mật khẩu mới đã được cài đặt thành công!')
-            return
-    password = tsd.askstring('Mật khẩu', 'Nhập mật khẩu', show='*')
-    if (password == key):
-        TrainImages()
-    elif (password == None):
-        pass
-    else:
-        mess._show(title='Error', message='Sai mật khẩu!')
-
-
-######################################################################################
-
-def clear():
-    txt.delete(0, 'end')
-    res = "1)Chụp hình  >>>  2)Lưu thông tin"
-    message1.configure(text=res)
-
-
-def clear2():
-    txt2.delete(0, 'end')
-    res = "1)Chụp hình  >>>  2)Lưu thông tin"
-    message1.configure(text=res)
-
-
-#######################################################################################
-
 def TakeImages():
-    check_haarcascadefile()
-    columns = ['SERIAL NO.', '', 'ID', '', 'NAME']
-    assure_path_exists("StudentDetails/")
-    assure_path_exists("TrainingImage/")
-    serial = 0
-    exists = os.path.isfile("StudentDetails/StudentDetails.csv")
-    if exists:
-        with open("StudentDetails/StudentDetails.csv", 'r') as csvFile1:
-            reader1 = csv.reader(csvFile1)
-            for l in reader1:
-                serial = serial + 1
-        serial = (serial // 2)
-        csvFile1.close()
-    else:
-        with open("StudentDetails/StudentDetails.csv", 'a+') as csvFile1:
-            writer = csv.writer(csvFile1)
-            writer.writerow(columns)
-            serial = 1
-        csvFile1.close()
-    Id = (txt.get())
-    name = (txt2.get())
-    if ((name.isalpha()) or (' ' in name)):
-        cam = cv2.VideoCapture(0)
-        harcascadePath = "haarcascade_frontalface_default.xml"
-        detector = cv2.CascadeClassifier(harcascadePath)
-        sampleNum = 0
-        while (True):
-            ret, img = cam.read()
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = detector.detectMultiScale(gray, 1.3, 5)
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                # incrementing sample number
-                sampleNum = sampleNum + 1
-                # saving the captured face in the dataset folder TrainingImage
-                cv2.imwrite("TrainingImage/ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg",
-                            gray[y:y + h, x:x + w])
-                # display the frame
-                cv2.imshow('Taking Images', img)
-            # wait for 100 miliseconds
-            if cv2.waitKey(100) & 0xFF == ord('q'):
-                break
-            # break if the sample number is morethan 100
-            elif sampleNum > 100:
-                break
-        cam.release()
-        cv2.destroyAllWindows()
-        res = "Images Taken for ID : " + Id
-        row = [serial, '', Id, '', name]
-        with open('StudentDetails/StudentDetails.csv', 'a+') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerow(row)
-        csvFile.close()
-        message1.configure(text=res)
-    else:
-        if (name.isalpha() == False):
-            res = "Nhập tên chính xác"
-            message.configure(text=res)
+    vid = cv2.VideoCapture(0)
 
+    while (True):
 
-########################################################################################
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
 
-def TrainImages():
-    check_haarcascadefile()
-    assure_path_exists("TrainingImageLabel/")
-    recognizer = cv2.face_LBPHFaceRecognizer.create()
-    harcascadePath = "haarcascade_frontalface_default.xml"
-    detector = cv2.CascadeClassifier(harcascadePath)
-    faces, ID = getImagesAndLabels("TrainingImage")
-    try:
-        recognizer.train(faces, np.array(ID))
-    except:
-        mess._show(title='Error', message='Xin hãy đăng kí một ai đó trước!')
-        return
-    recognizer.save("TrainingImageLabel/Trainner.yml")
-    res = "Thông tin được lưu thành công!"
-    message1.configure(text=res)
-    message.configure(text='Tổng lượt đã đăng kí  : ' + str(ID[0]))
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
 
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-############################################################################################3
-
-def getImagesAndLabels(path):
-    # get the path of all the files in the folder
-    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
-    # create empth face list
-    faces = []
-    # create empty ID list
-    Ids = []
-    # now looping through all the image paths and loading the Ids and the images
-    for imagePath in imagePaths:
-        # loading the image and converting it to gray scale
-        pilImage = Image.open(imagePath).convert('L')
-        # Now we are converting the PIL image into numpy array
-        imageNp = np.array(pilImage, 'uint8')
-        # getting the Id from the image
-        ID = int(os.path.split(imagePath)[-1].split(".")[1])
-        # extract the face from the training image sample
-        faces.append(imageNp)
-        Ids.append(ID)
-    return faces, Ids
-
-
-###########################################################################################
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
 
 def TrackImages():
-    check_haarcascadefile()
-    assure_path_exists("Attendance/")
-    assure_path_exists("StudentDetails/")
-    for k in tv.get_children():
-        tv.delete(k)
-    msg = ''
-    i = 0
-    j = 0
-    recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
-    exists3 = os.path.isfile("TrainingImageLabel/Trainner.yml")
-    if exists3:
-        recognizer.read("TrainingImageLabel/Trainner.yml")
-    else:
-        mess._show(title='Error', message='Xin hãy click vào nút Lưu thông tin để khởi tạo lại!')
-        return
-    harcascadePath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(harcascadePath);
+    vid = cv2.VideoCapture(0)
 
-    cam = cv2.VideoCapture(0)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    col_names = ['Id', '', 'Name', '', 'Date', '', 'Time']
-    exists1 = os.path.isfile("StudentDetails/StudentDetails.csv")
-    if exists1:
-        df = pd.read_csv("StudentDetails/StudentDetails.csv")
-    else:
-        mess._show(title='Error', message='Thiếu thông tin sinh viên, xin hãy kiểm tra lại!')
-        cam.release()
-        cv2.destroyAllWindows()
-        window.destroy()
-    while True:
-        ret, im = cam.read()
-        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.2, 5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(im, (x, y), (x + w, y + h), (225, 0, 0), 2)
-            serial, conf = recognizer.predict(gray[y:y + h, x:x + w])
-            if (conf < 50):
-                ts = time.time()
-                date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
-                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-                aa = df.loc[df['SERIAL NO.'] == serial]['NAME'].values
-                ID = df.loc[df['SERIAL NO.'] == serial]['ID'].values
-                ID = str(ID)
-                ID = ID[1:-1]
-                bb = str(aa)
-                bb = bb[2:-2]
-                attendance = [str(ID), '', bb, '', str(date), '', str(timeStamp)]
+    while (True):
 
-            else:
-                Id = 'Không xác định'
-                bb = str(Id)
-            cv2.putText(im, str(bb), (x, y + h), font, 1, (255, 255, 255), 2)
-        cv2.imshow('Điểm danh', im)
-        if (cv2.waitKey(1) == ord('q')):
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    ts = time.time()
-    date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
-    exists = os.path.isfile("Attendance/Attendance_" + date + ".csv")
-    if exists:
-        with open("Attendance/Attendance_" + date + ".csv", 'a+') as csvFile1:
-            writer = csv.writer(csvFile1)
-            writer.writerow(attendance)
-        csvFile1.close()
-    else:
-        with open("Attendance/Attendance_" + date + ".csv", 'a+') as csvFile1:
-            writer = csv.writer(csvFile1)
-            writer.writerow(col_names)
-            writer.writerow(attendance)
-        csvFile1.close()
-    with open("Attendance/Attendance_" + date + ".csv", 'r') as csvFile1:
-        reader1 = csv.reader(csvFile1)
-        for lines in reader1:
-            i = i + 1
-            if (i > 1):
-                if (i % 2 != 0):
-                    iidd = str(lines[0]) + '   '
-                    tv.insert('', 0, text=iidd, values=(str(lines[2]), str(lines[4]), str(lines[6])))
-    csvFile1.close()
-    cam.release()
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
     cv2.destroyAllWindows()
 
 
-######################################## USED STUFFS ############################################
+    ######################################## USED STUFFS ############################################
 
 global key
 key = ''
@@ -385,13 +108,12 @@ frame3.place(relx=0.52, rely=0.09, relwidth=0.09, relheight=0.07)
 frame4 = tk.Frame(window, bg="#c4c6ce")
 frame4.place(relx=0.36, rely=0.09, relwidth=0.16, relheight=0.07)
 
-datef = tk.Label(frame4, text=day + "-" + mont[month] + "-" + year + "  |  ", fg="black", bg="#f9f6f2", width=55,
+datef = tk.Label(frame4, text=day + "-" + mont[month] + "-" + year + "  |  ", fg="#f9f6f2", bg="#f9f6f2", width=55,
                  height=1, font=('comic', 22, ' bold '))
 datef.pack(fill='both', expand=1)
 
 clock = tk.Label(frame3, fg="black", bg="#f9f6f2", width=55, height=1, font=('comic', 22, ' bold '))
 clock.pack(fill='both', expand=1)
-tick()
 
 head2 = tk.Label(frame2, text="                            Đăng kí mới                              ", fg="black",
                  bg="#d1d1d1", font=('comic', 17, ' bold '))
@@ -441,7 +163,7 @@ message.configure(text='Tổng lượt đăng kí  : ' + str(res))
 
 menubar = tk.Menu(window, relief='ridge')
 filemenu = tk.Menu(menubar, tearoff=0)
-filemenu.add_command(label='Đổi mật khẩu', command=change_pass)
+filemenu.add_command(label='Đổi mật khẩu')
 filemenu.add_command(label='Thoát', command=window.destroy)
 menubar.add_cascade(label='Trợ giúp', font=('comic', 29, ' bold '), menu=filemenu)
 
@@ -466,16 +188,16 @@ tv.configure(yscrollcommand=scroll.set)
 
 ###################### BUTTONS ##################################
 
-clearButton = tk.Button(frame2, text="Xóa", command=clear, fg="black", bg="#f09ea7", width=11,
+clearButton = tk.Button(frame2, text="Xóa", fg="black", bg="#f09ea7", width=11,
                         activebackground="white", font=('comic', 11, ' bold '))
 clearButton.place(x=335, y=86)
-clearButton2 = tk.Button(frame2, text="Xóa", command=clear2, fg="black", bg="#f09ea7", width=11,
+clearButton2 = tk.Button(frame2, text="Xóa", fg="black", bg="#f09ea7", width=11,
                          activebackground="white", font=('comic', 11, ' bold '))
 clearButton2.place(x=335, y=172)
 takeImg = tk.Button(frame2, text="Chụp hình", command=TakeImages, fg="black", bg="#e1dbd6", width=34, height=1,
                     activebackground="white", font=('comic', 15, ' bold '))
 takeImg.place(x=30, y=300)
-trainImg = tk.Button(frame2, text="Lưu thông tin", command=psw, fg="black", bg="#e1dbd6", width=34, height=1,
+trainImg = tk.Button(frame2, text="Lưu thông tin", fg="black", bg="#e1dbd6", width=34, height=1,
                      activebackground="white", font=('comic', 15, ' bold '))
 trainImg.place(x=30, y=380)
 trackImg = tk.Button(frame1, text="Điểm danh", command=TrackImages, fg="black", bg="#c1ebc0", width=35, height=1,
